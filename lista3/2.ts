23bdfe0e928359b2b43c376d71ca81a1ee49b758
PedrOliveiraM <pedropucmont@gmail.com>
2024-08-24 23:23:47 -0300

    update lista 3

--- lista3/2.ts
+++ lista3/2.ts
@@ -7,23 +7,23 @@ const metrageTable: { [key: number]: number } = {
 }
 const neighborhoodTable: { [key: string]: number } = {
   "asa sul": 0,
-  "univeritario": 1,
+  "universitario": 1,
   "nova vila": 2,
   "zona leste": 3,
   "morumbi": 4,
 }
 
 let matrix: number[][] = new Array(5)
-
-let metrage: number = 150
+let metrage: number = 200
 let bairro: string = "universitario"
+
 
 
 // preenchendo
 for (let i = 0; i < 5; i++) {
   matrix[i] = new Array(5)
   for (let j = 0; j < 5; j++) {
-    matrix[i][j] = Math.trunc(Math.random() * 100000)
+    matrix[i][j] = Math.trunc(Math.random() * 1000)
   }
 }
 
@@ -50,5 +50,24 @@ function lowestPriceApartamentByMetrage(metrage: number) {
   }
   console.log("Menor valor da metragem ", metrage, " = ", menor)
 }
+function priceApartament(metrage: number, neighborhood: string) {
+  let row: number = metrageTable[metrage]
+  let col: number = neighborhoodTable[neighborhood]
+  console.log(row)
+  console.log(col)
+  console.log("Valor do apto: ", matrix[row][col])
+}
+
+function averageApartamentMetrage() {
+  let total: number = 0
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      total += matrix[j][i]
+    }
+    console.log("Media da ", i, "coluna = ", total / 5)
+  }
+}
 
 lowestPriceApartamentByMetrage(metrage)
+priceApartament(metrage, bairro)
+averageApartamentMetrage()
